f7654317f6188a9da78846fbc637ec32281f7173
Jamie Reid <wjreid85.jr@gmail.com>
2019-01-25 13:01:13 +0000

    Adds clock and letters.

---
+++ client/src/app.js
@@ -0,0 +1,18 @@
+console.log('JavaScript Loaded');
+document.addEventListener('DOMContentLoaded', () => {
+
+  const letters = document.querySelectorAll('h1');
+
+  const alphabet = ['A','B','C','D','E','F','G','H','I','J','H','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
+  letters.forEach((letter) => {
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * Math.floor(max));
+    }
+
+    letter.textContent = alphabet[getRandomInt(26)];
+  })
+
+  console.log(letters);
+
+});
