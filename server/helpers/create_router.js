c5240e8c9406ed3608b70c708c8a0f62d1955b6b
klkirstyleeskl <k.lees@btinternet.com>
2019-01-25 11:45:49 +0000

    adds few CRUD functions to database

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -14,14 +14,47 @@ const createRouter = function (collection) {
       console.err(err);
       res.status(500);
       res.json({ status: 500, error: err})
-    })
-  })
+    });
+  });
+
+  router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    collection
+    .findOne({_id: ObjectID(id) })
+    .then((docs)  => res.json(docs))
+    .catch((err) => {
+      console.err(err);
+      res.status(500);
+      res.json({ status: 500, error: err})
+    });
+  });
+
+  router.delete('/:id', (req, res) => {
+      const id = req.params.id;
+      collection
+        .deleteOne({ _id: ObjectID(id) })
+        .then(() => collection.find().toArray())
+        .then((docs) => res.json(docs))
+        .catch((err) => {
+          console.error(err);
+          res.status(500);
+          res.json({ status: 500, error: err });
+        });
+    });
+
+  router.post('/', (req, res) => {
+    const newData = req.body;
+    collection
+      .insertOne(newData)
+      .then(() => collection.find().toArray())
+      .then((docs) => res.json(docs));
+  });
+
+  
 
 
-  return router
+return router;
 
-
-}
-
+};
 
 module.exports = createRouter;
