3d4ff331db13605e7926d90db429cb4c430a5dd4
Jamie Reid <wjreid85.jr@gmail.com>
2019-01-30 21:03:00 +0000

    Styles word input elements.

--- client/src/views/letters_game_view.js
+++ client/src/views/letters_game_view.js
@@ -27,28 +27,49 @@ LettersGameView.prototype.showLetters = function () {
 
   const wordSubmitContainer = document.createElement('div');
   wordSubmitContainer.id = "word-submit";
-  for (let i = 0; i<2;i++) {
-    const form = document.createElement('form');
-    form.id = `p${i+1}-word-submit`;
-    const label = document.createElement('label');
-    label.for = "word";
-    label.textContent = "Submit your word";
+  // for (let i = 0; i<2;i++) {
+    const formP1 = document.createElement('form');
+    formP1.id = `p1-word-submit`;
+    const formP2 = document.createElement('form');
+    formP2.id = `p2-word-submit`;
+    // const label = document.createElement('label');
+    // label.for = "word";
+    // label.placeholder = "Player 1 submit your word";
 
-    const input1 = document.createElement('input');
-    input1.type = "text";
-    input1.id = "word";
-    input1.required=true;
+    const inputP1 = document.createElement('input');
+    inputP1.type = "text";
+    inputP1.id = "p1WordInput";
+    inputP1.required = true;
+    inputP1.placeholder = "Player 1 submit your word";
 
-    const input2 = document.createElement('input');
-    input2.type = "submit";
-    input2.value = "save";
+    const inputP2 = document.createElement('input');
+    inputP2.type = "text";
+    inputP2.id = "p2WordInput";
+    inputP2.required = true;
+    inputP2.placeholder = "Player 2 submit your word";
 
-    form.appendChild(label);
-    form.appendChild(input1);
-    form.appendChild(input2);
-    wordSubmitContainer.appendChild(form);
 
-    this.elements.push(form);
+    const inputP1Btn = document.createElement('input');
+    inputP1Btn.type = "submit";
+    inputP1Btn.id = "p1-word-input-button";
+    inputP1Btn.value = "submit";
+
+    const inputP2Btn = document.createElement('input');
+    inputP2Btn.type = "submit";
+    inputP2Btn.id = "p2-word-input-button";
+    inputP2Btn.value = "submit";
+
+    // form.appendChild(label);
+    formP1.appendChild(inputP1);
+    formP1.appendChild(inputP1Btn);
+    wordSubmitContainer.appendChild(formP1);
+
+    formP2.appendChild(inputP2Btn);
+    formP2.appendChild(inputP2);
+    wordSubmitContainer.appendChild(formP2);
+
+    this.elements.push(formP1);
+    this.elements.push(formP2);
 
     // const jokeContainer = document.querySelector('#joke-container');
 
@@ -61,7 +82,7 @@ LettersGameView.prototype.showLetters = function () {
     this.container.appendChild(lettersContainer);
     this.container.appendChild(wordSubmitContainer);
 
-}
+// }
   PubSub.subscribe('Joke:joke-loaded', (evt) => {
     const jokeElement = document.createElement('p');
     console.log(evt);
@@ -85,10 +106,10 @@ LettersGameView.prototype.setupEventListener = function () {
       let word2;
 
       this.elements[0].addEventListener('submit', function(event) {
-
+        console.log(event);
         event.preventDefault();
         const form = event.target;
-        word1 = event.target.word.value;
+        word1 = event.target.p1WordInput.value;
 
         form.reset();
 
@@ -101,7 +122,7 @@ LettersGameView.prototype.setupEventListener = function () {
 
         event.preventDefault();
         const form = event.target;
-        word2 = event.target.word.value;
+        word2 = event.target.p2WordInput.value;
         PubSub.publish(`LettersGameView:submitted-word-p1-round-${round}`, word1);
         PubSub.publish(`LettersGameView:submitted-word-p2-round-${round}`, word2);
 
